b92801477895665ad0af64b6d196c2f6a91847e0
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-05-03 20:56:19 +0200

    add functions to the interface

--- app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
@@ -1,5 +1,17 @@
 package com.example.mad03_fragments_and_navigation.database
 
+import androidx.lifecycle.LiveData
+import com.example.mad03_fragments_and_navigation.models.Movie
+
 interface MovieDao {
-    // TODO implement me
+
+    fun create(movie: Movie): Long
+
+    fun update(movie: Movie)
+
+    fun delete(movieId: Long)
+
+    fun clearTable()
+
+    fun getAll(): LiveData<List<Movie>>
 }
