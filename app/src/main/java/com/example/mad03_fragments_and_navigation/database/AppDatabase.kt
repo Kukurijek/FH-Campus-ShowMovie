b22a3540f26207c5bc30af5b777c07276461db99
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-05-03 22:10:15 +0200

    use Movie data class

--- app/src/main/java/com/example/mad03_fragments_and_navigation/database/AppDatabase.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/database/AppDatabase.kt
@@ -1,7 +1,15 @@
 package com.example.mad03_fragments_and_navigation.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.mad03_fragments_and_navigation.models.Movie
 
+@Database(
+    entities = [Movie::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase: RoomDatabase() {
-    // TODO implement me
+    abstract val appDatabase: MovieDao
 }
+
--- app/src/main/java/com/example/mad03_fragments_and_navigation/models/Movie.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/models/Movie.kt
@@ -2,35 +2,36 @@ package com.example.mad03_fragments_and_navigation.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
-import com.example.mad03_fragments_and_navigation.R
 
+@Entity(tableName = "movie_table")
 data class Movie(
+
+    @ColumnInfo(name = "movie_title")
     var title: String = "",
-    var description: String = ""
+
+  //  var description: String = ""
 ) {
+    @PrimaryKey(autoGenerate = true)
     var id: Long? = 0L
+/*
     var rating: Float = 0.0F
         set(value) {
             if(value in 0.0..5.0) field = value
             else throw IllegalArgumentException("Rating value must be between 0 and 5")
-        }
-    var note: String = ""
-    var imageId: Int = R.drawable.no_preview_3
-    var actors: MutableList<String> = mutableListOf()
-    var creators: MutableList<String> = mutableListOf()
-    var genres: List<String>? = null
-}
-
-@Entity(tableName = "movie_table")
-data class Ajde(
-
-    @ColumnInfo(name = "movie_title")
-    var title: String = "",
-) {
-    @PrimaryKey(autoGenerate = true)
-    var id: Long? = 0L
-
+        } */
     @ColumnInfo(name = "movie_note")
     var note: String = ""
- }
+
+//    var imageId: Int = R.drawable.no_preview_3
+
+    @Ignore
+    var actors: MutableList<String> = mutableListOf()
+
+    @Ignore
+    var creators: MutableList<String> = mutableListOf()
+
+    @Ignore
+    var genres: List<String>? = null
+}
