4fbb74d7be697b1003eb2523f12d2202c1fa6b78
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-05-22 12:21:56 +0200

    add few functions to repository

--- app/src/main/java/com/example/mad03_fragments_and_navigation/repositories/MovieRepository.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/repositories/MovieRepository.kt
@@ -1,9 +1,37 @@
 package com.example.mad03_fragments_and_navigation.repositories
 
 import com.example.mad03_fragments_and_navigation.database.MovieDao
+import com.example.mad03_fragments_and_navigation.models.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MovieRepository(private val movieDao: MovieDao) {
     // TODO implement me
+
+    /**
+    suspend fun clearTable() {
+            withContext(Dispatchers.IO) {
+                movieDao.
+            }
+    }
+    **/
+
+    suspend fun updateTable(movieToUpdate: Movie) {
+        withContext(Dispatchers.IO) {
+            movieDao.run {
+                update(movie = movieToUpdate)
+            }
+        }
+    }
+
+    suspend fun insertTable(movieToCreate: Movie) {
+        withContext(Dispatchers.IO) {
+            movieDao.run {
+                create(movie = movieToCreate)
+            }
+        }
+    }
+
 
     companion object {
         // For Singleton instantiation
