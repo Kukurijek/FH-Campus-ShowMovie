c41fa6173a3bed145b891c468fb25f1f889f2a16
Kukurijek <nemanja.filipovic@stud.fh-campuswien.ac.at>
2021-05-23 02:24:07 +0200

    add all methods

--- app/src/main/java/com/example/mad03_fragments_and_navigation/viewmodels/MovieFavoritesViewModel.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/viewmodels/MovieFavoritesViewModel.kt
@@ -1,10 +1,42 @@
 package com.example.mad03_fragments_and_navigation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mad03_fragments_and_navigation.models.Movie
 import com.example.mad03_fragments_and_navigation.repositories.MovieRepository
+import kotlinx.coroutines.launch
 
 class MovieFavoritesViewModel (
     private val repository: MovieRepository
 ): ViewModel() {
-    // TODO implement me
+
+    fun clearTable() {
+        viewModelScope.launch {
+            repository.clearTable()
+        }
+    }
+
+    fun deleteMovie(id: Long) {
+        viewModelScope.launch {
+            repository.delete(movieId = id)
+        }
+    }
+
+    fun gettAll() {
+        viewModelScope.launch {
+            repository.getAll()
+        }
+    }
+
+    fun insertMovie(movie: Movie) {
+        viewModelScope.launch {
+            repository.insertTable(movieToCreate = movie)
+        }
+    }
+
+    fun updateMovie(movie: Movie) {
+        viewModelScope.launch {
+            repository.updateTable(movieToUpdate = movie)
+        }
+    }
 }
